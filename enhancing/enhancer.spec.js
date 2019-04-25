dab26a3725191891daa3eb6a6f4b3f608ed8d551
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-24 18:42:38 -0700

    Add Repair Function and Test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+const { repair } = require("./enhancer.js");
+// - a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
+describe("repair function", () => {
+  it("should return an item with a durability score of 100", () => {
+    const item = {
+      name: "Golden Gun",
+      type: "weapon",
+      durability: 5
+    };
+
+    expect(repair(item).durability).toEqual(100);
+  });
+});
